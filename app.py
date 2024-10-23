e1d092d609771a260374905c2de2d87ad62d7696
Emile <emile.merle@epita.fr>
2024-10-23 16:30:41 +0200

    feat: change ssh behavior

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ model = joblib.load("regression.joblib")
 @app.get("/predict")
 async def predict(data: Dict):
     price = model.predict([data['size'], data['nb_bedrooms'], data['has_garden']])[0]
-    st.write(f"The price of the house is {price}.")
+    return {"price": price}
 
